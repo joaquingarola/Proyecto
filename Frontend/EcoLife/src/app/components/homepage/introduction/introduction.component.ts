88098672d6a6b4fef74681f79c44b2c5dfb895ae
joaquingarola <98132472+joaquingarola@users.noreply.github.com>
2024-08-25 20:36:57 -0300

    Image carousel hp

--- Frontend/EcoLife/src/app/components/homepage/introduction/introduction.component.ts
+++ Frontend/EcoLife/src/app/components/homepage/introduction/introduction.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './introduction.component.scss'
 })
 export class IntroductionComponent {
-
+  photos = [
+    { imageSrc: '../../../../assets/hp-carousel/carousel1.png', altText: 'Foto 1' },
+    { imageSrc: '../../../../assets/hp-carousel/carousel2.png', altText: 'Foto 2' },
+    { imageSrc: '../../../../assets/hp-carousel/carousel4.png', altText: 'Foto 4' },
+    { imageSrc: '../../../../assets/hp-carousel/carousel5.png', altText: 'Foto 5' },
+    { imageSrc: '../../../../assets/hp-carousel/carousel6.png', altText: 'Foto 6' },
+  ];
 }
